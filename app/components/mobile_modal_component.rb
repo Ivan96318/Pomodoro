2bdcc650ace8d5517716c434617a5772a9e50fb2
Ivan Aguilar <ivan96318@gmail.com>
2025-06-08 21:09:48 -0700

    finish mobile modal component

--- app/components/mobile_modal_component.rb
+++ app/components/mobile_modal_component.rb
@@ -1,5 +1,20 @@
 class MobileModalComponent < ViewComponent::Base
-  def initialize(title:)
+renders_one :header
+renders_one :content
+renders_one :footer
+
+  def initialize(title:, **options)
     @title = title
+    @options = options
+  end
+
+  peivate
+
+  attr_reader :title, :options
+
+  def css_classes
+    classes = ["native-modal-overlay"]
+    classes << options[:class] if options[:class]
+    classes.join(" ")
   end
 end
